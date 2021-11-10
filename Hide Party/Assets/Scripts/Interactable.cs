59b6c1a010ef7645056fa6cafd36c4a10eff44d6
Laura Huovinen <laura.huovinen@outlook.com>
2021-11-10 13:56:46 +0200

    Player can interact with objects
    - Just a base though
    - If player comes into the object's radius, scripts communicate with each others and object can be interacted
    - If player leaves the object's radius, player loses its "focus" - player can't interact with the object anymore

--- Hide Party/Assets/Scripts/Interactable.cs
+++ Hide Party/Assets/Scripts/Interactable.cs
@@ -9,6 +9,7 @@ public class Interactable : MonoBehaviour
     [SerializeField] public float radius = 2f;
     [SerializeField] private TextMeshPro textMesh;
 
+    private InteractionController pcController;
     public bool canInteract;
 
     void Update()
@@ -21,28 +22,52 @@ public class Interactable : MonoBehaviour
     {
         RaycastHit2D hit = Physics2D.CircleCast(transform.position, radius, Vector2.zero);
 
-        if (hit == false)
+        if (hit)
+        {
+            pcController = hit.collider.GetComponent<InteractionController>();
+
+            if (pcController != null)  // Was the hit caused by the player and if so, do stuff
+            {
+                textMesh.gameObject.SetActive(true);
+                canInteract = true;
+
+                // Gives the player a chance to interact with it
+                pcController.SetFocus(gameObject);
+            }
+            else
+            {
+                textMesh.gameObject.SetActive(false);
+                canInteract = false;
+            }
+        }
+        else if (pcController != null)  // Checks if the ray isn't hitting the object anymore
         {
             textMesh.gameObject.SetActive(false);
-            return;
-        }
+            canInteract = false;
 
-        if (hit.collider.gameObject.layer == 3)
-        {
-            textMesh.gameObject.SetActive(true);
-            canInteract = true;
+            // Prevents the player from interacting with the object after leaving the ray cast area
+            pcController.RemoveFocus();
+            pcController = null;
         }
         else
         {
             textMesh.gameObject.SetActive(false);
             canInteract = false;
         }
+
     }
 
-    void Interact()
+    public void Interact()
     {
-        // Do the interaction
-        Debug.Log("Hey, you interacted with me!");
+        if (canInteract)
+        {
+            // Do the interaction
+            Debug.Log("Hey, you interacted with me!");
+        }
+        else
+        {
+            Debug.Log("You don't have a permission to interact with me");
+        }
     }
 
     private void OnDrawGizmosSelected()
