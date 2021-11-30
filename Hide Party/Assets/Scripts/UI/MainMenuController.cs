62686ac5569a0bb3d1114891816d91e75525ba51
Laura Huovinen <laura.huovinen@outlook.com>
2021-11-30 15:10:35 +0200

    Functional main menu base
    - Doesn't load a level yet but otherwise works
    - Needs some graphics

---
+++ Hide Party/Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Play pressed");
+        //SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit pressed");
+        Application.Quit();
+    }
+}
