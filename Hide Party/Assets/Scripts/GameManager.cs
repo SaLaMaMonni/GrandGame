78924670d2aa7444d2c738d5d7862741d21aff81
Laura Huovinen <laura.huovinen@outlook.com>
2021-12-13 13:47:28 +0200

    Stuff for build?
    - Just need to commit to merge with other branches

--- Hide Party/Assets/Scripts/GameManager.cs
+++ Hide Party/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -45,6 +46,9 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
